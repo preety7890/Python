4e3b21b1a99eb4f58822dabc1edfeb6b7dee12c5
Preety Gupta <preety6388@gmail.com>
2026-06-07 21:31:38 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,29 @@
+name=input("Enter your name:")
+rollNo=int(input("Enter your roll number:"))
+course=input("Enter your course:")
+College=input("Enter your college name:")
+fees=int(input("Enter your fees:"))
+Address=input("Enter your address:")
+
+print("Name:",name )
+print("RollNo:",rollNo)
+print("Course:",course )
+print("College:",College )
+print("Fees:",fees )
+print("Address:",Address )
+
+# create product details program :id,name,price,rating,is_available
+product_id=101
+Name="iphone"
+price=78000
+rating=4.2
+is_available=True
+
+print("product_id:",product_id)
+print("Name:",Name)
+print("price:",price)
+print("rating:",rating)
+print("is available:",is_available)
+
+
+
